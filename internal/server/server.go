80de60dfd05c5c4069c4043cfe626eeff5003778
Dmego <dmeago@gmail.com>
2026-09-22 17:10:32 +0800

    chore: bump version to 1.3.1

--- internal/server/server.go
+++ internal/server/server.go
@@ -112,7 +112,7 @@ func (a *API) handleVersion(w http.ResponseWriter, r *http.Request) {
 }
 
 // Version is the build version reported by /version and the admin service view.
-const Version = "1.3.0"
+const Version = "1.3.1"
 
 func (a *API) planeState() string {
 	if a.Plane == nil {
